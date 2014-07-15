af1bff450f92bc1d87855b7aa9c5cc3674939733
Yoonsung Ahn <yyssahn@hotmail.com>
2014-07-15 15:01:52 -0700

    first commit

---
+++ PeerAX/src/com/example/peerax/StartScreenActivity.java
@@ -0,0 +1,15 @@
+package com.example.peerax;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class StartScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+    }
+}
